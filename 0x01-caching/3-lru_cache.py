2a756cab06264c32872f8c4b6768b2b5529d381e
Twayfrancis <tawaifrancis@gmail.com>
2024-01-02 19:07:06 +0300

    class LRUCache that inherits from BaseCaching and is a caching system:
    You must use self.cache_data - dictionary from the parent class BaseCaching
    You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
    def put(self, key, item):
    Must assign to the dictionary self.cache_data the item value for the key key.
    If key or item is None, this method should not do anything.
    If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
    you must discard the least recently used item (LRU algorithm)
    you must print DISCARD: with the key discarded and following by a new line
    def get(self, key):
    Must return the value in self.cache_data linked to key.
    If key is None or if the key doesn’t exist in self.cache_data, return None.

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""class LRUCache that inherits from BaseCaching and is a caching system"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    LRUCache class that inherits from BaseCaching.
+    This class represents a caching system using LRU algorithm.
+    """
+
+    def __init__(self):
+        """
+        Initialize the class.
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        Assign the item value for the key in self.cache_data dictionary.
+
+        Parameters:
+        key (str): key where the item will be cached.
+        item (str): the item to be cached.
+
+        Returns:
+        None
+        """
+        if key is None or item is None:
+            return
+        if key in self.keys:
+            self.keys.remove(key)
+        self.keys.append(key)
+        self.cache_data[key] = item
+        if len(self.keys) > BaseCaching.MAX_ITEMS:
+            discarded_key = self.keys.pop(0)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key.
+
+        Parameters:
+        key (str): key for which we need the value.
+
+        Returns:
+        str: Value linked to the key, None if key doesn't exist.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
