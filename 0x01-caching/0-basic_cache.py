a204c7b9e94c6b2b289c031f0e05a41e31c02066
Twayfrancis <tawaifrancis@gmail.com>
2024-01-02 18:34:04 +0300

    class BasicCache that inherits from BaseCaching and is a caching system:
    You must use self.cache_data - dictionary from the parent class BaseCaching
    This caching system doesn’t have limit
    def put(self, key, item):
    Must assign to the dictionary self.cache_data the item value for the key key.
    If key or item is None, this method should not do anything.
    def get(self, key):
    Must return the value in self.cache_data linked to key.
    If key is None or if the key doesn’t exist in self.cache_data, return None.

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Class BasicCache that inherits from BaseCaching and is a caching sys"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """"""
+    def put(self, key, item):
+        """
+        Assign the item value for the key in self.cache_data dict
+
+        Parameters:
+            key (str): Key where the item will be cached
+            item (str): the item to be cached.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        retyrn the value in self.cache_data linked to key
+
+        Parameters:
+            key (str): key for which we need the value
+
+        Returns:
+            str: value linked to the key, None if the key doesn't exist.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
