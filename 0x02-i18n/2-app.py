6c7c2870f07aa5bfb0123c79ab063013f6c9b5df
Twayfrancis <tawaifrancis@gmail.com>
2024-01-09 15:18:43 +0300

    fix function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -20,6 +20,7 @@ app.config.from_object('1-app.Config')
 
 @babel.localeselector
 def get_locale():
+    """get locale function"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
